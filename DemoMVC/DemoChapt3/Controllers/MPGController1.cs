fe95e09b3566d07793b3092efbb072caf6c63beb
Hamberfim <adhamlin@gmail.com>
2024-02-23 13:46:50 -0600

    start MPG

---
+++ DemoMVC/DemoChapt3/Controllers/MPGController1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoChapt3.Controllers
+{
+    public class MPGController1 : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.MilesDriven = 0;
+            ViewBag.GallonsUsed = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(MPGController1 driverStats)
+        {
+            return View(driverStats);
+        }
+    }
+}
+}
---
+++ DemoMVC/DemoChapt3/Models/MPG.cs
@@ -0,0 +1,28 @@
+﻿namespace DemoChapt3.Models
+{
+    public class MPG
+    {
+        private double _milesDriven;
+        private double _gallonsUsed;
+
+        public double MilesDriven
+        {
+            get => _milesDriven;
+            set
+            {
+                _milesDriven = value;
+            }
+        }
+
+        public double GallonsUsed
+        {
+            get => _gallonsUsed;
+            set
+            {
+                _gallonsUsed = value;
+            }
+        }
+    }
+
+    
+}
