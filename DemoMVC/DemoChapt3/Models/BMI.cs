6731690ddb3cfbdb6dd7c1d4d06a436776c329ad
Hamberfim <adhamlin@gmail.com>
2024-02-23 15:32:31 -0600

    model

---
+++ DemoMVC/DemoChapt3/Models/BMI.cs
@@ -0,0 +1,50 @@
+﻿namespace DemoChapt3.Models
+{
+    public class BMI
+    {
+        private double _weight;
+        private double _height;
+
+        public double Weight
+        {
+            get { return _weight; }
+            set { _weight = value; }
+        }
+
+        public double Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+        public double CalcBmi()
+        {
+            return _weight / (_height * _height);
+
+        }
+
+        public string getCategory()
+        {
+            string category = "";
+            double _bmi = CalcBmi();
+            if (_bmi < 18.5)
+            {
+                category = "underweight";
+            }
+            else if (_bmi < 25)
+            {
+                category = "healthy weight";
+            }
+            else if (_bmi < 30)
+            {
+                category = "over weight";
+            }
+            else
+            {
+                category = "obese";
+            }
+            return category;
+        }
+
+    }
+}
