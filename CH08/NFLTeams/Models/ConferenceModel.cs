71a42b774fb1d927fe81fa9eaad1fa13edcf99d1
Hamberfim <adhamlin@gmail.com>
2024-02-24 10:12:17 -0600

    setup and models

---
+++ CH08/NFLTeams/Models/ConferenceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NFLTeams.Models
+{
+    public class ConferenceModel
+    {
+        public string ConferenceId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
---
+++ CH08/NFLTeams/Models/DivisionModel.cs
@@ -0,0 +1,8 @@
+﻿namespace NFLTeams.Models
+{
+    public class DivisionModel
+    {
+        public string DivisionId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+    }
+}
